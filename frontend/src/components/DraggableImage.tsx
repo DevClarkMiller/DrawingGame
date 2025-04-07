0a6a2230bb643f0548c8ed5b6218ac62587a8d8a
DevClarkMiller <clarkmillermail@gmail.com>
2025-04-06 23:29:42 -0400

    Added in the draggable image frame component. Now just need to implement the ability to resize the frame

--- frontend/src/components/DraggableImage.tsx
+++ frontend/src/components/DraggableImage.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 // Components
 import Draggable from 'react-draggable';
 
+// Icons
+import { RxCross2 } from "react-icons/rx";
+import { VscChromeMinimize } from "react-icons/vsc";
+
 function DraggableImage() {
+    const nodeRef = useRef<HTMLDivElement>(null);
+
+    // State
+    const [minimized, setMinimized] = useState<boolean>(false);
+
     return (
-        <Draggable><div>DraggableImage</div></Draggable>
+        <Draggable nodeRef={nodeRef as React.RefObject<HTMLElement>}>
+            <div className='hover:cursor-pointer z-50 border-gray-500 border-2 rounded' ref={nodeRef}>
+                <div className='imageHeader bg-gray-500 flex justify-end w-full text-white'>
+                    <button onClick={() => setMinimized(!minimized)} className='hover:text-blue-500 clean-btn'>
+                        <VscChromeMinimize className='text-2xl nice-trans'/>
+                    </button>
+                </div>
+                {!minimized ?
+                    <>
+                        <div className=''>
+                            <img src='https://www.exitlag.com/blog/wp-content/uploads/2024/12/Who-Is-Steve-in-Minecraft_-The-Story-Behind-the-Iconic-Character.webp'/>
+                        </div>
+                    </> : <div className='p-2'>Image</div>
+                }
+            </div>
+        </Draggable>
     );
 }
 
