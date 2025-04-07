cd3d33e99927251bcd8dd349b81034c271215d0a
DevClarkMiller <clarkmillermail@gmail.com>
2025-04-06 23:39:54 -0400

    Added in re-resizable to be able to resize the window, next thing needed is to fix its import

--- frontend/src/components/DraggableImage.tsx
+++ frontend/src/components/DraggableImage.tsx
@@ -2,9 +2,9 @@ import React, { useRef, useState } from 'react'
 
 // Components
 import Draggable from 'react-draggable';
+import { Resizable } from 're-resizable';
 
 // Icons
-import { RxCross2 } from "react-icons/rx";
 import { VscChromeMinimize } from "react-icons/vsc";
 
 function DraggableImage() {
@@ -14,20 +14,26 @@ function DraggableImage() {
     const [minimized, setMinimized] = useState<boolean>(false);
 
     return (
-        <Draggable nodeRef={nodeRef as React.RefObject<HTMLElement>}>
+        <Draggable 
+            nodeRef={nodeRef as React.RefObject<HTMLElement>}
+            bounds="body"
+        >
             <div className='hover:cursor-pointer z-50 border-gray-500 border-2 rounded' ref={nodeRef}>
-                <div className='imageHeader bg-gray-500 flex justify-end w-full text-white'>
-                    <button onClick={() => setMinimized(!minimized)} className='hover:text-blue-500 clean-btn'>
-                        <VscChromeMinimize className='text-2xl nice-trans'/>
-                    </button>
-                </div>
-                {!minimized ?
-                    <>
-                        <div className=''>
-                            <img src='https://www.exitlag.com/blog/wp-content/uploads/2024/12/Who-Is-Steve-in-Minecraft_-The-Story-Behind-the-Iconic-Character.webp'/>
-                        </div>
-                    </> : <div className='p-2'>Image</div>
-                }
+                <Resizable
+                >
+                    <div className='imageHeader bg-gray-500 flex justify-end w-full text-white'>
+                        <button onClick={() => setMinimized(!minimized)} className='hover:text-blue-500 clean-btn'>
+                            <VscChromeMinimize className='text-2xl nice-trans'/>
+                        </button>
+                    </div>
+                    {!minimized ?
+                        <>
+                            <div className=''>
+                                <img src='https://www.exitlag.com/blog/wp-content/uploads/2024/12/Who-Is-Steve-in-Minecraft_-The-Story-Behind-the-Iconic-Character.webp'/>
+                            </div>
+                        </> : <div className='p-2'>Image</div>
+                    }
+                </Resizable>
             </div>
         </Draggable>
     );
